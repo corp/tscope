5c4d9f73118e3277fdfac0b5ad1ba0dfe6a92744
Alejandro Corpeno <alejandro.corpeno@gmail.com>
2014-01-20 18:35:58 -0600

    add the twitter gem

--- Gemfile
+++ Gemfile
@@ -7,6 +7,7 @@ gem 'rails', '4.0.2'
 # Use sqlite3 as the database for Active Record
 # gem 'sqlite3'
 gem 'pg'
+gem "twitter", "~> 5.5.1"
 
 # Use SCSS for stylesheets
 gem 'sass-rails', '~> 4.0.0'
--- app/models/keyword.rb
+++ app/models/keyword.rb
@@ -1,2 +1,14 @@
 class Keyword < ActiveRecord::Base
+  def grab_twitts
+    client = Twitter::REST::Client.new do |config|
+     config.consumer_key = "ct2UMxQ6LeljhB2Zz4VDOg"
+     config.consumer_secret = "h9ObYorDoqOpNl11yKLf1OMh16HZAZphXzv5PReJw"
+     config.oauth_token = "14077884-1MthRU9sIrSAtxSmFP0wy1EXma9SYh56G4nAk2bwc"
+     config.oauth_token_secret = "QrmNAdoqsUshHLxGGy8yuUqmHaEmUvbnO5SyrVnlM"
+    end
+    
+    client.search(self.word, :count => 3, :result_type => "recent").take(3).collect do |tweet|
+      "#{tweet.user.screen_name}: #{tweet.text}"
+    end
+  end
 end
