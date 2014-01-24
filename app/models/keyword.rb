89e2b41d20b1e85dd883d2035b587da092556231
Alejandro Corpeno <alejandro.corpeno@gmail.com>
2014-01-24 17:00:30 -0600

    use config vars for twitter keys

--- app/models/keyword.rb
+++ app/models/keyword.rb
@@ -3,10 +3,10 @@ class Keyword < ActiveRecord::Base
   
   def grab_twitts
     client = Twitter::REST::Client.new do |config|
-     config.consumer_key = "ct2UMxQ6LeljhB2Zz4VDOg"
-     config.consumer_secret = "h9ObYorDoqOpNl11yKLf1OMh16HZAZphXzv5PReJw"
-     config.oauth_token = "14077884-1MthRU9sIrSAtxSmFP0wy1EXma9SYh56G4nAk2bwc"
-     config.oauth_token_secret = "QrmNAdoqsUshHLxGGy8yuUqmHaEmUvbnO5SyrVnlM"
+     config.consumer_key = ENV["TWITTER_CONSUMER_KEY"]
+     config.consumer_secret = ENV["TWITTER_CONSUMER_SECRET"]
+     config.oauth_token = ENV["TWITTER_OAUTH_TOKEN"]
+     config.oauth_token_secret = ENV["TWITTER_OAUTH_SECRET"]
     end
     
     client.search(self.word, :count => 100, :result_type => "recent").take(100).collect do |tweet|
