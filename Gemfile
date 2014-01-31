33a85676dc90f2d70c7520a51b31833702742098
Alejandro Corpeno <alejandro.corpeno@gmail.com>
2014-01-31 09:09:26 -0600

    asset sync installed

--- Gemfile
+++ Gemfile
@@ -19,6 +19,7 @@ gem 'mini_magick'
 # Use SCSS for stylesheets
 gem 'sass-rails', '~> 4.0.0'
 gem 'bootstrap-sass', '~> 3.1.0'
+gem 'asset_sync'
 
 # Use Uglifier as compressor for JavaScript assets
 gem 'uglifier', '>= 1.3.0'
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -56,6 +56,7 @@ Tweetscope::Application.configure do
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.action_controller.asset_host = "http://assets.example.com"
+  config.action_controller.asset_host = "//#{ENV['AWS_BUCKET']}.s3.amazonaws.com"
 
   # Precompile additional assets.
   # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
