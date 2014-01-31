e3ad80a7db04ab0da7c6ea4ae60fc03f16259433
Alejandro Corpeno <alejandro.corpeno@gmail.com>
2014-01-31 09:41:06 -0600

    using asset sync

--- config/initializers/asset_sync.rb
+++ config/initializers/asset_sync.rb
@@ -12,11 +12,11 @@ AssetSync.configure do |config|
   #
   # Automatically replace files with their equivalent gzip compressed version
   # config.gzip_compression = true
-  #
+  
   # Use the Rails generated 'manifest.yml' file to produce the list of files to
   # upload instead of searching the assets directory.
   # config.manifest = true
-  #
+  
   # Fail silently.  Useful for environments such as Heroku
   # config.fail_silently = true
 end
